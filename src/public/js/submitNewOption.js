5b08329ca588fb7e437009fe5b698db4f6bdf25e
Gus <gustavoaca1997@gmail.com>
2017-12-31 04:08:57 -0400

    fixed jquery bug for adding option to existed poll

--- src/public/js/submitNewOption.js
+++ src/public/js/submitNewOption.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
+	let adding = false;
 	$('#btnAddOption').click(function() {
-		$('#new-option').removeClass('d-none');
-		$('#btnAddOption').html('Cancel');
-		$('#submit-button').html('Vote for new option');
-
-		$('#btnAddOption').click(function() {
+		if (!adding) {
+			$('#new-option').removeClass('d-none');
+			$('#btnAddOption').html('Cancel');
+			$('#submit-button').html('Vote for new option');
+			adding = true;
+		} else {
 			$('#new-option').addClass('d-none');
 			$('#btnAddOption').html('Add Option');
 			$('#submit-button').html('Submit');
-		});
-	})
+			adding = false;
+		}	
+	});
 	$('#submit-button').click(function() {
-		if ($('#btnAddOption').html() === 'Cancel') {
+		if (adding) {
 			$('#poll-options').append('<li class="d-none list-group-item input-group py-1 px-2">'+
 				'<input type="radio" name="poll" value="'+
 				$('#new-option input').val()+
